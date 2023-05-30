c4bd39f4e91f680ab82ebfda82e4ac058580bd9f
Denis Anicai <denisanicai14@gmail.com>
2023-05-30 19:43:33 +0300

    update

--- NaturaHorti/settings.py
+++ NaturaHorti/settings.py
@@ -1,3 +1,8 @@
+import dotenv
+import os
+
+dotenv.load_dotenv(override=True)
+
 """
 Django settings for NaturaHorti project.
 
@@ -19,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-5iw29n)47)&g0qb2t+w_k*=ayu$mf)g!e)+zqbt@&2-4hqrgvc'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -115,11 +120,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATIC_URL = '/Frontend/static/'
+STATIC_ROOT = BASE_DIR / 'static'
 
 MEDIA_ROOT = BASE_DIR / 'static/images'
 
