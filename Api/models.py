6d87369ac9972669ea755ff210febff097206796
Denis Anicai <denisanicai14@gmail.com>
2023-06-05 23:51:52 +0300

    fix

--- Api/models.py
+++ Api/models.py
@@ -66,7 +66,7 @@ class OrderItem(models.Model):
     name = models.CharField(max_length=200, null=True, blank=True)
     qty = models.IntegerField(default=0)
     price = models.DecimalField(max_digits=7, decimal_places=2)
-    image = models.CharField(max_length=200, null=True, blank=True)
+    image = models.CharField(max_length=2500, null=True, blank=True)
     _id = models.AutoField(primary_key=True, editable=False)
 
     def __str__(self):
